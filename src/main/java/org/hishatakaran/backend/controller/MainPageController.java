bf7c5a2724b115e7b3b999bb1bd604929fc27fd5
Narek <narekgharagyozyann@gmail.com>
2026-08-01 22:12:02 +0400

    add main page logic

---
+++ src/main/java/org/hishatakaran/backend/controller/MainPageController.java
@@ -0,0 +1,37 @@
+package org.hishatakaran.backend.controller;
+
+import org.hishatakaran.backend.model.LibraryRequestDto;
+import org.hishatakaran.backend.model.LibraryResponseDto;
+import org.hishatakaran.backend.model.MainPageRequestDto;
+import org.hishatakaran.backend.model.MainPageResponseDto;
+import org.hishatakaran.backend.service.MainPageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MainPageController {
+
+  private final MainPageService mainPageService;
+
+  @PostMapping("/admin/mainPage")
+  public ResponseEntity<MainPageResponseDto> editMainPage(
+      @RequestBody MainPageRequestDto mainPageRequestDto
+  ){
+
+    return ResponseEntity.ok(mainPageService.editMainPage(mainPageRequestDto));
+
+  }
+
+  @GetMapping("/mainPage")
+  public ResponseEntity<MainPageResponseDto> editMainPage() {
+    return ResponseEntity.ok(mainPageService.getMainPage());
+  }
+}
