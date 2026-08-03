b704b63bf391b0776cd3cbbbed23e97d4808623f
Narek <narekgharagyozyann@gmail.com>
2026-08-04 02:03:02 +0400

    change quantity endpoint

--- src/main/java/org/hishatakaran/backend/service/MainPageService.java
+++ src/main/java/org/hishatakaran/backend/service/MainPageService.java
@@ -1,9 +1,9 @@
 package org.hishatakaran.backend.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.hishatakaran.backend.entity.MainPage;
+import org.hishatakaran.backend.entity.MonumentStatus;
 import org.hishatakaran.backend.mapper.MainPageMapper;
 import org.hishatakaran.backend.model.MainPageEditDto;
 import org.hishatakaran.backend.model.MainPageRequestDto;
@@ -16,8 +16,8 @@ import org.hishatakaran.backend.model.SettlementsCountDto;
 import org.hishatakaran.backend.repository.MainPageRepository;
 import org.hishatakaran.backend.repository.MonumentImageRepository;
 import org.hishatakaran.backend.repository.MonumentRepository;
+import org.hishatakaran.backend.repository.MonumentStatusRepository;
 import org.hishatakaran.backend.repository.MonumentVideoRepository;
-import org.hishatakaran.backend.repository.ProgramRepository;
 import org.hishatakaran.backend.repository.SettlementRepository;
 import org.springframework.stereotype.Service;
 
@@ -31,14 +31,14 @@ public class MainPageService {
 
   private final MainPageRepository mainPageRepository;
   private final MonumentRepository monumentRepository;
-  private final ProgramRepository programRepository;
   private final MonumentImageRepository monumentImageRepository;
   private final MonumentVideoRepository monumentVideoRepository;
+  private final MonumentStatusRepository monumentStatusRepository;
   private final SettlementRepository settlementRepository;
   private final GeminiService geminiService;
 
 
-  public MainPageResponseDto editMainPage(MainPageRequestDto mainPageRequestDto) {
+  public MainPageResponseDto createMainPage(MainPageRequestDto mainPageRequestDto) {
     List<MainPage> all = mainPageRepository.findAll();
     if (all.size() > 1) {
       throw new RuntimeException("Something went wrong");
@@ -93,10 +93,18 @@ public class MainPageService {
   public QuantityResponseDto getQuantity() {
     Long imagesCount = monumentImageRepository.countMonumentImages();
     Long videosCount = monumentVideoRepository.countMonumentVideos();
+    MonumentStatus registeredMonumentStatus = monumentStatusRepository.findById(1).orElseThrow(
+        () -> new RuntimeException("Something went wrong"));
+    MonumentStatus documentedMonumentStatus = monumentStatusRepository.findById(2).orElseThrow(
+        () -> new RuntimeException("Something went wrong"));
     return new QuantityResponseDto(
         new MonumentCountDto(
-            monumentRepository.countMonuments(),
-            monumentRepository.getMonumentTypeCounts()
+            monumentRepository.countMonuments(registeredMonumentStatus),
+            monumentRepository.getMonumentTypeCounts(registeredMonumentStatus)
+        ),
+        new MonumentCountDto(
+            monumentRepository.countMonuments(documentedMonumentStatus),
+            monumentRepository.getMonumentTypeCounts(documentedMonumentStatus)
         ),
         new SettlementsCountDto(
             settlementRepository.countSettlements(),
