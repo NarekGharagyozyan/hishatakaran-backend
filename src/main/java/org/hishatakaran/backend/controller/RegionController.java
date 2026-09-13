8fa0da43211b1fe9de18162b1b2d9fb8bac25658
Narek <narekgharagyozyann@gmail.com>
2026-09-13 17:12:43 +0400

    add crud for regions

--- src/main/java/org/hishatakaran/backend/controller/RegionController.java
+++ src/main/java/org/hishatakaran/backend/controller/RegionController.java
@@ -1,6 +1,8 @@
 package org.hishatakaran.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.hishatakaran.backend.model.RegionEditDto;
+import org.hishatakaran.backend.model.RegionRequestDto;
 import org.hishatakaran.backend.model.RegionResponseDto;
 import org.hishatakaran.backend.service.RegionService;
 import org.springframework.web.bind.annotation.*;
@@ -8,19 +10,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/regions")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class RegionController {
 
     private final RegionService regionService;
 
-    @GetMapping
+    @GetMapping("/regions")
     public List<RegionResponseDto> getAll() {
         return regionService.getAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/regions/{id}")
     public RegionResponseDto getById(@PathVariable Long id) {
         return regionService.getById(id);
+    }
+
+    @PostMapping("/admin/regions")
+    public RegionResponseDto addNewRegion(
+        @RequestBody RegionRequestDto regionRequestDto
+    ) {
+        return regionService.createNewRegion(regionRequestDto);
+    }
+
+    @PutMapping("/admin/regions/{regionId}")
+    public RegionResponseDto editRegion(
+        @PathVariable Long regionId,
+        @RequestBody RegionEditDto regionEditDto
+    ) {
+        return regionService.editRegion(regionId, regionEditDto);
+    }
+
+    @DeleteMapping("/admin/regions/{regionId}")
+    public void deleteRegion(@PathVariable Long regionId) {
+        regionService.deleteRegion(regionId);
     }
 }
--- src/main/java/org/hishatakaran/backend/service/RegionService.java
+++ src/main/java/org/hishatakaran/backend/service/RegionService.java
@@ -2,12 +2,25 @@ package org.hishatakaran.backend.service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.hishatakaran.backend.entity.Monument;
+import org.hishatakaran.backend.entity.Region;
+import org.hishatakaran.backend.entity.Settlement;
+import org.hishatakaran.backend.exception.SomethingWentWrongException;
 import org.hishatakaran.backend.mapper.RegionMapper;
+import org.hishatakaran.backend.model.RegionEditDto;
+import org.hishatakaran.backend.model.RegionRequestDto;
 import org.hishatakaran.backend.model.RegionResponseDto;
+import org.hishatakaran.backend.model.RegionTranslationDto;
+import org.hishatakaran.backend.repository.MonumentRepository;
 import org.hishatakaran.backend.repository.RegionRepository;
+import org.hishatakaran.backend.repository.SettlementRepository;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -15,6 +28,9 @@ import lombok.RequiredArgsConstructor;
 public class RegionService {
 
     private final RegionRepository regionRepository;
+    private final SettlementRepository settlementRepository;
+    private final MonumentRepository monumentRepository;
+    private final GeminiService geminiService;
 
     public List<RegionResponseDto> getAll() {
         return regionRepository.findAll()
@@ -28,5 +44,59 @@ public class RegionService {
         return RegionMapper.toDto(
             regionRepository.findById(id).orElseThrow()
         );
+    }
+
+    public RegionResponseDto createNewRegion(RegionRequestDto regionRequestDto) {
+        RegionTranslationDto translation;
+
+        try {
+            translation = geminiService.translateRegion(regionRequestDto.getName());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to translate region");
+        }
+
+        Region region = new Region();
+        region.setNameHy(translation.getNameHy());
+        region.setNameEn(translation.getNameEn());
+        region.setNameFr(translation.getNameFr());
+
+        return RegionMapper.toDto(regionRepository.save(region));
+    }
+
+    public RegionResponseDto editRegion(Long regionId, RegionEditDto regionEditDto) {
+        Region region = regionRepository.findById(regionId)
+            .orElseThrow(() -> new RuntimeException("Region not found"));
+
+        region.setNameHy(regionEditDto.getName().getHy());
+        region.setNameEn(regionEditDto.getName().getEn());
+        region.setNameFr(regionEditDto.getName().getFr());
+
+        return RegionMapper.toDto(regionRepository.save(region));
+    }
+
+    @Transactional
+    public void deleteRegion(Long regionId) {
+        Region region = regionRepository.findById(regionId)
+            .orElseThrow(() -> new RuntimeException("Region not found"));
+
+        List<Settlement> settlementsInRegion = settlementRepository.findAllByRegionId(regionId);
+        if (!settlementsInRegion.isEmpty()) {
+            throw new SomethingWentWrongException("Կան բնակավայրեր որոնք օգտագործում են տվյալ մարզը։ Այդ բնակավայրերն են՝ "
+                + settlementsInRegion.stream()
+                .map(Settlement::getNameHy)
+                .collect(Collectors.joining(", "))
+            );
+        }
+
+        List<Monument> monumentsInRegion = monumentRepository.findByRegionId(regionId);
+        if (!monumentsInRegion.isEmpty()) {
+            throw new SomethingWentWrongException("Կան հուշարձաններ որոնք օգտագործում են տվյալ մարզը։ Այդ հուշարձաններն են՝ "
+                + monumentsInRegion.stream()
+                .map(Monument::getNameHy)
+                .collect(Collectors.joining(", "))
+            );
+        }
+
+        regionRepository.delete(region);
     }
 }
