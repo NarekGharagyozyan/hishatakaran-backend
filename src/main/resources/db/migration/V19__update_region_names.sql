c332dd90cd14b155ba894fc8c67432bb4f1ca14a
Narek <narekgharagyozyann@gmail.com>
2026-09-17 20:11:44 +0400

    change region names

---
+++ src/main/resources/db/migration/V19__update_region_names.sql
@@ -0,0 +1,44 @@
+UPDATE regions
+SET name_en = 'Askeran Region',
+    name_fr = 'Région d’Askeran'
+WHERE id = 1;
+
+UPDATE regions
+SET name_en = 'Hadrut Region',
+    name_fr = 'Région de Hadrout'
+WHERE id = 2;
+
+UPDATE regions
+SET name_en = 'Martakert Region',
+    name_fr = 'Région de Martakert'
+WHERE id = 3;
+
+UPDATE regions
+SET name_en = 'Martuni Region',
+    name_fr = 'Région de Martouni'
+WHERE id = 4;
+
+UPDATE regions
+SET name_en = 'Nor Shahumyan (Karvachar) Region',
+    name_fr = 'Région de Nor Chahoumyan (Kʻarvachaṛ)'
+WHERE id = 5;
+
+UPDATE regions
+SET name_en = 'Shahumyan Region',
+    name_fr = 'Région de Chahoumyan'
+WHERE id = 6;
+
+UPDATE regions
+SET name_en = 'Shushi Region',
+    name_fr = 'Région de Chouchi'
+WHERE id = 7;
+
+UPDATE regions
+SET name_en = 'Stepanakert',
+    name_fr = 'Stepanakert'
+WHERE id = 8;
+
+UPDATE regions
+SET name_en = 'Kashatagh Region (Kʻashatʻagh)',
+    name_fr = 'Région de Kashatagh (Kʻashatʻagh)'
+WHERE id = 9;
